bdfe281a8b3176f41c0a71135c2a6bfeef8f26c4
Connor-Hughes-Nes <chughes@neslotech.co.za>
2022-11-21 18:32:17 +0200

    Add Login file and app logo

--- src/components/authentication/Login.jsx
+++ src/components/authentication/Login.jsx
@@ -1,1 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Input } from 'src/components/commonComponents/Input.jsx'
+
+const serverify = ({ email, password }) => ({
+  email,
+  password
+})
+
+const Login = ({}) => {
+  const [form, setForm] = useState(undefined);
+  const handleChange = (newState) => setForm({ ...form, ...newState });
+
+  return (
+    <section className="login">
+      <form className="form">
+        <Input
+          name="email"
+          label="Email Address"
+          placeholder="Email Address"
+          type="email"
+          value={form?.email}
+          onChange={handleChange}
+        />
+        <Input
+          name="password"
+          label="Password"
+          placeholder="Password"
+          value={form?.password}
+          onChange={handleChange}
+        />
+      </form>
+    </section>
+  )
+};
+
+export default Login;
---
+++ src/components/commonComponents/Input.jsx
@@ -0,0 +1,39 @@
+import React, { createRef, useMemo } from 'react';
+
+export const Input = ({
+  id,
+  name,
+  label,
+  onChange,
+  value
+  // hidden
+}) => {
+  const wrapperRef = createRef();
+
+  const getValueChange = (name, value) => (name ? { [name]: value } : value);
+
+  const memoLabel = useMemo(() => {
+    return label;
+  }, [label]);
+
+  const handleChange = (e) => {
+    let val = e.target.value;
+    val = getValueChange(name, val)
+    onChange(val);
+  }
+
+  return (
+    <div className="input">
+      <fieldset ref={wrapperRef} className="input__input-wrapper">
+        <input
+          id={id ?? name}
+          onChange={handleChange}
+          name={name}
+          value={value}
+          placeholder={memoLabel}
+          // hidden={hidden}
+        />
+      </fieldset>
+    </div>
+  );
+};

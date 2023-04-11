829887e264237bf2ea64b6f74babc5e4fe22b6b5
Connor-Hughes-Nes <chughes@neslotech.co.za>
2023-04-11 15:56:45 +0200

    Clean up budger reducer

--- src/reducers/budget.reducer.js
+++ src/reducers/budget.reducer.js
@@ -6,7 +6,7 @@ export const initialState = {
   budget: undefined
 };
 
-//The function you pass to reduce is known as a “reducer”.
+// The function you pass to reduce is known as a “reducer”.
 // It takes the result so far and the current item, then it returns the next result.
 // React reducers are an example of the same idea: they take the state so far and the action,
 // and return the next state. In this way, they accumulate actions over time into state.
